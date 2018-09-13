1d8b33022e337463f65f1f49b6bf8f7f6684f512
yeo <173486632@qq.com>
2018-09-13 15:06:55 +0800

    commit

--- client/pages/components/imgAbbreviation/index.js
+++ client/pages/components/imgAbbreviation/index.js
@@ -4,17 +4,5 @@ Component({
       type: Object,
       value: {}
     }
-  },
-  data: {
-    // 这里是一些组件内部数据
-    someData: {}
-  },
-  methods: {
-    // 这里是一个自定义方法
-    customMethod() {
-      this.setData({
-        "someData": this.properties.foodData
-      })
-    }
   }
 })
