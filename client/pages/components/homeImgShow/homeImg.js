35f8376324287cbf91c5d6446847ee24ffef3b13
cc.huang <cc.huang@uxin.com>
2018-09-14 10:28:45 +0800

    首页样式

--- client/pages/components/homeImgShow/homeImg.js
+++ client/pages/components/homeImgShow/homeImg.js
@@ -14,12 +14,19 @@ Component({
     // 这里是一些组件内部数据
     someData: {}
   },
+  onShow: function () {
+    console.log('132')
+  },
   methods: {
     // 这里是一个自定义方法
     customMethod() {
       this.setData({
         "someData": this.properties.foodData
       })
+    },
+    clickLikeContent() {
+      console.log('123');
+      
     }
   }
 })
